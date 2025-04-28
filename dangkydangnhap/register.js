e4e85e86416466273a0b5fccd95cd6a138a38f94
Tiendat3007 <phamhuynhtiendatpy@gmail.com>
2025-04-28 10:01:10 +0700

    buoi_4

--- dangkydangnhap/register.js
+++ dangkydangnhap/register.js
@@ -1,21 +1,52 @@
 function registerUser() {
-    const fullName = document.getElementById('full_name').value;
-    const firstName = document.getElementById('first_name').value;
+    const fullName = document.getElementById('full_name').value.trim();
+    const firstName = document.getElementById('first_name').value.trim();
     const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm_password').value;
-    const agreeTerms = document.getElementById('agree_terms').checked;
+    const password = document.getElementById('password').value.trim();
+    const confirmPassword = document.getElementById('confirm_password').value.trim();
+   /* check thong tin nguoi dung nhap vao*/
+
+    const emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (fullName === '') {
+        alert('Họ và tên đệm không được để trống!');
+        return;
+    }
+
+    if (firstName === '') {
+        alert('Tên không được để trống!');
+        return;
+    }
+
+    if (email === '') {
+        alert('Email không được để trống!');
+        return;
+    }
+
+    if (!emailCheck.test(email)) {
+        alert('Email không hợp lệ! Vui lòng nhập đúng định dạng.');
+        return;
+    }
+
+    if (password.trim() === '') {
+        alert('Mật khẩu không được để trống!');
+        return;
+    }
+   
+    if (password.length < 8) {
+        alert('Mật khẩu phải có ít nhất 8 ký tự!');
+        return;
+    }
+
+    if (confirmPassword.trim() === '') {
+        alert('xác nhận mật khẩu không được để trống!');
+        return;
+    }
 
     if (password !== confirmPassword) {
         alert('Mật khẩu không khớp!');
         return;
     }
-
-    if (!agreeTerms) {
-        alert('Bạn phải đồng ý với chính sách và điều khoản!');
-        return;
-    }
-
     // Tạo user object
     const user = {
         fullName: `${fullName} ${firstName}`,
